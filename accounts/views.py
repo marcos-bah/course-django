83ec3c79ec005a697e0cbb9a7d4d1bc07b9f4753
marcos-bah <marcos.barbosa@unifei.edu.br>
2020-05-30 22:12:54 -0300

    criado urls e views [pt2]

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(req):
+    return HttpResponse('home')
+
+def products(req):
+    return HttpResponse('products')
+
+def customer(req):
+    return HttpResponse('customer')
+
+
