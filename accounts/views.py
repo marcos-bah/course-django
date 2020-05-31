b042f044ea6b55ac52b0ca2d4fad9819c06d126c
marcos-bah <marcos.barbosa@unifei.edu.br>
2020-05-30 23:11:19 -0300

    criaçao dos templates [pt3]

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(req):
-    return HttpResponse('home')
+    return render(req, 'accounts/dashboard.html')
 
 def products(req):
-    return HttpResponse('products')
+    return render(req, 'accounts/products.html')
 
 def customer(req):
-    return HttpResponse('customer')
+    return render(req, 'accounts/customer.html')
 
 
