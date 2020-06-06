0e7ca923236f09d74b437437ff0696b4a26c66ad
marcos-bah <marcos.barbosa@unifei.edu.br>
2020-06-06 20:29:27 -0300

    passando variaveis pelas views

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def home(req):
-    return render(req, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count() 
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    return render(req, 'accounts/dashboard.html', {
+        'orders': orders, 
+        'customers': customers, 
+        'total_orders': total_orders, 
+        'total_customers': total_customers,
+        'delivered': delivered,
+        'pending' : pending,
+    })
 
 def products(req):
-    return render(req, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(req, 'accounts/products.html', {'products': products})
 
 def customer(req):
     return render(req, 'accounts/customer.html')
